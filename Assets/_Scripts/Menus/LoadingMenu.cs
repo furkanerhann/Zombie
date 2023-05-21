9d1a3c9e79ad8a5ed8b1858724e06a8e75fc3ead
Furkan Erhan <79505626+furkanerhann@users.noreply.github.com>
2023-05-21 23:57:24 +0300

    General Update For Scenes

---
+++ Assets/_Scripts/Menus/LoadingMenu.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class LoadingMenu : MonoBehaviour
+{
+    public GameObject loadingScreen;
+    public GameObject buttonToClose;
+    public Image loadingBarFill;
+    public void LoadScreen()
+    {
+        StartCoroutine(LoadSceneAsync());
+    }
+    IEnumerator LoadSceneAsync()
+    {
+        loadingScreen.SetActive(true);
+        buttonToClose.SetActive(false);
+        loadingBarFill.fillAmount = 0.0f;
+        yield return new WaitForSeconds(1f);
+        loadingBarFill.fillAmount = 0.2f;
+        yield return new WaitForSeconds(1f);
+        loadingBarFill.fillAmount = 0.5f;
+        yield return new WaitForSeconds(2f);
+        loadingBarFill.fillAmount = 0.9f;
+        yield return new WaitForSeconds(1f);
+        AsyncOperation operation = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+        while (!operation.isDone)
+        {
+            float progressValue = Mathf.Clamp01(operation.progress / 0.9f);
+            loadingBarFill.fillAmount = progressValue;
+            yield return null;
+        }
+    }
+}
