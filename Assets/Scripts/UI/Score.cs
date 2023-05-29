f065c8f1578bb000bc74f93583a33c65361b56dc
Mustafa Tüfekçi <44066965+mustafa-tufekci@users.noreply.github.com>
2023-05-29 13:05:53 +0300

    high score on dead screen complete

--- Assets/Scripts/UI/Score.cs
+++ Assets/Scripts/UI/Score.cs
@@ -6,20 +6,35 @@ public class Score : MonoBehaviour
 {
     public int scoreAmountOnKill;
     public int currentScore;
+    public int highScore;
     [SerializeField] private Text scoreText;
+    [SerializeField] private Text highScoreText;
     void Start()
     {
-        
+        InitVariables();
+        UpdateHighScore();
     }
 
     private void InitVariables()
     {
         currentScore = 0;
+        highScore = PlayerPrefs.GetInt("HighScore", 0);
     }
 
     public void AddScore()
     {
         currentScore += scoreAmountOnKill;
         scoreText.text = currentScore.ToString();
+
+        if (currentScore > highScore)
+        {
+            highScore = currentScore;
+            PlayerPrefs.SetInt("HighScore", highScore);
+            UpdateHighScore();
+        }
+    }
+    private void UpdateHighScore()
+    {
+        highScoreText.text = highScore.ToString();
     }
 }
